9030d882c37cbaa7119f6b4a8663476a9b70b4a9
khoaHyh <khoahuynh.toronto@gmail.com>
2021-02-08 20:33:26 -0500

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -4,7 +4,9 @@ var app = express();
 console.log("Hello World");
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
-})
+});
+
+app.use(express.static(__dirname + "/public"));
 
 
 
