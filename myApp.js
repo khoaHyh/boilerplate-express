6628816c549575ed0b50ba289997fabfca302a9a
khoaHyh <khoahuynh.toronto@gmail.com>
2021-02-08 20:47:50 -0500

    Fix: Use the .env file
    Remove accidental object literal wrap on res.json.

--- myApp.js
+++ myApp.js
@@ -3,18 +3,18 @@ var app = express();
 require('dotenv').config();
 
 console.log("Hello World");
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/views/index.html");
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + "/views/index.html");
+// });
 
-app.use(express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + "/public"));
 
 app.get('/json', (req, res) => {
     let response = {"message": "Hello json"};
     if (process.env.MESSAGE_STYLE === 'uppercase') {
         response.message = response.message.toUpperCase();
     }
-    res.json({response});
+    res.json(response);
 });
 
 
