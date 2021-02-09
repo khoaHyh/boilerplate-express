bcf97aa9a54f361529d4bad6a3db749b43f16a10
khoaHyh <khoahuynh.toronto@gmail.com>
2021-02-08 20:51:17 -0500

    Fix: Add sendfile block back

--- myApp.js
+++ myApp.js
@@ -3,11 +3,11 @@ var app = express();
 require('dotenv').config();
 
 console.log("Hello World");
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + "/views/index.html");
-// });
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
 
-// app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 
 app.get('/json', (req, res) => {
     let response = {"message": "Hello json"};
