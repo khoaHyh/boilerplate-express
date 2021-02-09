1e6c97f65866e076edd5ddc5c7c3a51d15e2ea20
khoaHyh <khoahuynh.toronto@gmail.com>
2021-02-08 23:03:07 -0500

    Docs: Add comments to label logic of each block

--- myApp.js
+++ myApp.js
@@ -3,24 +3,32 @@ var app = express();
 var bodyParser = require('body-parser');
 require('dotenv').config();
 
+/* 7. Implement a Root-Level Request Logger Middleware */
 app.use((req, res, next) => {
     console.log(req.method + " " + req.path + " - " + req.ip);
     next();
 });
 
+
+/* 11. Use body-parser to Parse POST Requests */
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}));
 
 // parse application/json
 app.use(bodyParser.json());
 
+/* 1. Meet the Node console */
 console.log("Hello World");
+
+/* 3. Serve an HTML File */
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
 
+/* 4. Serve Static Assets */
 app.use(express.static(__dirname + "/public"));
 
+/* 6. Use the .env File */
 app.get('/json', (req, res) => {
     let response = { "message": "HELLO JSON" };
     if (process.env.MESSAGE_STYLE === 'uppercase') {
@@ -29,6 +37,7 @@ app.get('/json', (req, res) => {
     res.status(200).json(response);
 });
 
+/* 8. Chain Middleware to create a Timer Server */
 app.get('/now', (req, res, next) => {
     req.time = new Date().toString();
     next();
@@ -36,14 +45,17 @@ app.get('/now', (req, res, next) => {
     res.status(200).json({ "time": req.time });
 });
 
+/* 9. Get Route Parameter Input from the Client */
 app.get('/:word/echo', (req, res) => {
     res.status(200).json({ echo: req.params.word });
 });
 
+/* 10. Get Query Parameter Input from the Client */
 app.route('/name')
     .get((req, res) => {
         res.status(200).json({ name: `${req.query.first} ${req.query.last}`});
     })
+    /* 12. Get Data from POST Requests */
     .post((req, res) => {
         res.status(200).json({ name: `${req.body.first} ${req.body.last}`});
     });
