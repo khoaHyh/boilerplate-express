c7a35c6c8428a710d569fb7c89cf61fb77e568b2
khoaHyh <khoahuynh.toronto@gmail.com>
2021-02-08 21:36:42 -0500

    Fix: Last challenge submission

--- myApp.js
+++ myApp.js
@@ -29,7 +29,7 @@ app.get('/now', (req, res, next) => {
     res.status(200).json({ "time": req.time });
 });
 
-app.get(':word/echo', (req, res) => {
+app.get('/:word/echo', (req, res) => {
     res.status(200).json({ echo: req.params.word });
 });
 
